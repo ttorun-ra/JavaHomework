bfa33305cc1bb52ce778a58d66943d89de48c4bb
yvzydn <yavuzaydinus@gmail.com>
2019-11-05 13:42:20 -0500

    last

---
+++ TechnoHomework/src/HomeworkDay6/HomeworkStrings.java
@@ -0,0 +1,58 @@
+package HomeworkDay6;
+
+public class HomeworkStrings {
+
+	public static void main(String[] args) {
+//	    What is the numerical range of a char data type in Java?
+//	    a) -128 to 127
+//	    b) 0 to 256
+//	    c) 0 to 32767
+//	    d) 0 to 65535
+		
+	//Answer:A;
+	//
+//	    What is the output of this program?
+	//
+//	    class mainclass {
+//	        public static void main(String args[])
+//	        {
+//	            char a = 'A';
+//	            a++;
+//	            System.out.print((int)a);
+//	        }
+//	    }
+//	    a) 66
+//	    b) 67
+//	    c) 65
+//	    d) 64
+	////Answer:A;
+	//
+//	    What is the output of this program?
+	//
+//	    class asciicodes {
+//	        public static void main(String args[])
+//	        {
+//	            char var1 = 'A';
+//	            char var2 = 'a';
+//	            System.out.println((int)var1 + " " + (int)var2);
+//	        }
+//	    }
+//	    a) 162
+//	    b) 65 97
+//	    c) 67 95
+//	    d) 66 98
+
+	//
+	//Answer:A
+	//
+//	    Which of these methods can be used to convert all characters in a String into a character array?
+//	    a) charAt()
+//	    b) both getChars() & charAt()
+//	    c) both toCharArray() & getChars()
+//	    d) All of the mentioned
+
+//		
+//		Answer: D
+	}
+
+}
--- TechnoHomework/src/HomeworkDay6/hw.java
+++ TechnoHomework/src/HomeworkDay6/hw.java
@@ -3,8 +3,73 @@ package HomeworkDay6;
 public class hw {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//	    1. What is the range of short data type in Java?
+//	    a) -128 to 127
+//	    b) -32768 to 32767
+//	    c) -2147483648 to 2147483647
+//	    d) None of the mentioned
+	//
+//		Answer:B
+		
+	//
+	//
+//	    2. What is the range of byte data type in Java?
+//	    a) -128 to 127
+//	    b) -32768 to 32767
+//	    c) -2147483648 to 2147483647
+//	    d) None of the mentioned
+//	    View Answer
+	//
+	//Answer:A
+	//
+//	    3. Which of the following are legal lines of Java code?
+//	    1. int w = (int)888.8;
+//	    2. byte x = (byte)100L;
+//	    3. long y = (byte)100;
+//	    4. byte z = (byte)100L;
 
+//	    a) 1 and 2
+//	    b) 2 and 3
+//	    c) 3 and 4
+//	    d) All statements are correct.
+	//
+	//
+	//
+//	    What is the output of this program?
+	//
+//	    class increment {
+//	        public static void main(String args[])
+//	        {
+//	            int g = 3;
+//	            System.out.print(++g * 8);
+//	        }
+//	    }
+//	    a) 25
+//	    b) 24
+//	    c) 32
+//	    d) 33
+	//
+//		Answer:C
+	//
+//	    What is the output of this program?
+	//
+//	    class area {
+//	        public static void main(String args[])
+//	        {
+//	            double r, pi, a;
+//	            r = 9.8;
+//	            pi = 3.14;
+//	            a = pi * r * r;
+//	            System.out.println(a);
+//	        }
+//	    }
+//	    a) 301.5656
+//	    b) 301
+//	    c) 301.56
+//	    d) 301.56560000
+
+//		
+//		Answer:A
 	}
 
 }
