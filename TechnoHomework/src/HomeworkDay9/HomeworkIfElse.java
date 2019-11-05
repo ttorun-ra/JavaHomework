3987b41c874133067ecb9df2572ac5e6cf4530df
yvzydn <yavuzaydinus@gmail.com>
2019-11-05 14:01:16 -0500

    day 9

---
+++ TechnoHomework/src/HomeworkDay9/HomeworkIfElse.java
@@ -0,0 +1,319 @@
+package HomeworkDay9;
+
+public class HomeworkIfElse {
+
+	public static void main(String[] args) {
+		 // highlight ctrl + /
+//	    public static void main(String[] args) {
+//	        int x = 10;
+//	
+//	        Boolean myVal = false;
+//	//
+//	        if(myVal==true)
+//	        if(myVal){
+//	//
+//	        }
+//	
+//	        if (x) {
+//	            System.out.println("HELLO TECHNO");
+//	        } else {
+//	            System.out.println("BYE");
+//	        }
+//	    }
+	////Options:
+	////1. HELLO TECHNO
+	////2. Compile time error
+	////3. Runtime Error
+	////4. BYE
+	
+	
+//	Answer:2
+		
+		
+	//
+//	    public class Test {
+//	        public static void main(String[] args)
+//	        {
+//	            int x = 10;
+//	            if (x)
+//	                System.out.println("HELLO TECHNO");
+//	            System.out.println("WELCOME");
+	//
+//	        else
+//	            {
+//	                System.out.println("BYE");
+//	            }
+//	        }
+//	    }
+	////Options:
+	////1. HELLO TECHNO
+	////WELCOME
+	////2. HELLO TECHNO
+	////3. BYE
+	////4. Compile time error
+	//
+		
+//		Answer:4
+		
+		
+		
+//	    public class Test {
+//	    public static void main(String[] args) {
+//	        int x = 10, y = 20;
+	//
+//	        if (x < y)
+//	            int a = 10;
+//	         else {
+//	            System.out.println("BYE");
+//	        }
+//	    }
+//	    }
+	////Options:
+	////1. 10
+	////2. BYE
+	////3. NO output
+	////4. Compile time error
+	//
+//		Answer:4
+		
+		
+//	    public class Test {
+//	        public static void main(String[] args)
+//	        {
+//	            int x = 10, y = 20;
+//	            //if(10 < 20)
+//	            if (x < y) {
+	//
+//	                //if(10 > 20)
+//	                if (x > y) {
+//	                    System.out.println("HELLO TECHNO");
+//	                } else {
+//	                    System.out.println("WELCOME");
+//	                }
+//	            }
+//	        }
+//	    }
+	////Output:
+	////1. HELLO TECHNO
+	////2. Compile time error
+	////3. WELCOME
+	////4. No Output
+	//
+//		Answer:2
+		
+		
+		
+//	    public class Test {
+//	        public static void main(String[] args)
+//	        {
+//	            if (true)
+//	                ;
+//	        }
+//	    }
+	////Options:
+	////1. No Output
+	////2. Compile time error
+	////3. Runtime error
+	////4. Runtime Exception
+		
+		
+		
+//	    class Test {
+	    //Mac command + slash
+	    //pc ctrl + /
+//	        public static void main(String[] args)
+//	        {
+//	            String day = "Sunday";
+//	            switch (day) {
+//	                case "Monday":
+//	                    System.out.println("Let's Work");
+//	                    break;
+//	                case "Saturday":
+//	                    System.out.println("waiting for Sunday");
+//	                    break;
+//	                case "Sunday":
+//	                    System.out.println("Today is fun day");
+//	            }
+//	        }
+//	    }
+	////Options:
+	////1. Compile time error
+	////2. Lets work
+	////3. Run- time error
+	////4. Today is fun day
+	//
+	
+
+//	Answer:4
+	
+	
+	
+//	    public class MainClass {
+
+	    //        enum day { MON,
+//	            SAT,
+//	            SUN }
+	//
+//	            public static void main(String[] args)
+//	        {
+//	            day ch = day.SUN;
+//	            switch (ch) {
+//	                case MON:
+//	                    System.out.println("Lets work!");
+//	                    break;
+//	                case SAT:
+//	                    System.out.println("Waiting for sunday");
+//	                    break;
+//	                case SUN:
+//	                    System.out.println("Lets have fun!");
+//	                    break;
+//	            }
+//	        }
+//	    }
+	////Options:
+	////1. No Output
+	////2. Syntax error
+	////3. Lets have fun!
+	////4. Compile time error
+
+
+//Answer:2
+
+	//
+//	    class MainClass {
+//	    public static void main(String[] args) {
+//	        int x = 10;
+//	
+//	        Switch (x) {
+//	            System.out.println("TECHNO");
+//	        }
+//	    }
+//	    }
+	////Options:
+	////1. TECHNO
+	////2. Compile time error
+	////3. No Output
+	////4. Run-time error
+
+//		Answer:2
+	//
+//	    class MainClass {
+//	        public static void main3(String[] args)
+//	        {
+//	            int x = 10;
+//	            int y = 20;
+//	            switch (x) {
+//	                case 10:
+//	                    System.out.println("HELLO");
+//	                    break;
+//	                case 85:
+//	                    System.out.println("TECHNO");
+//	                    break;
+//	            }
+//	        }
+//	    }
+	////Options:
+	////1. HELLO
+	////2. No Output
+	////3. TECHNO
+	////4. Compile time error
+	//
+
+
+//Answer:1
+
+//	    class MainClass {
+//	    public static void main(String[] args) {
+//	        int x = 10;
+//	        final int y = 20;
+//	        switch (x) {
+//	            case 10:
+//	                System.out.println("HELLO");
+//	                break;
+//	            case y:
+//	                System.out.println("TECHNO");
+//	                break;
+//	        }
+//	    }
+//	    }
+	////Options:
+	////1. TECHNO
+	////2. Compile time error
+	////3. HELLO
+	////4. NO Output
+	////Output:
+	//
+//Answer:3
+//	    class MainClass {
+	//
+//	        public static void main(String[] args)
+//	        {
+//	            int x = 10;
+//	            switch (x + 1 + 1) {
+//	                case 10:
+//	                    System.out.println("HELLO");
+//	                    break;
+//	                case 12:
+//	                    System.out.println("TECHNO");
+//	                    break;
+//	            }
+//	        }
+//	    }
+	////Output:
+	////1. Compile time error
+	////2. TECHNO
+	////3. HELLO
+	////4. No Output
+	//
+
+//		Answer:2
+		
+//	    class MainClass {
+//	        public static void main(String arg[])
+//	        {
+//	            char stream = 'C';
+//	            int x = 2;
+//	
+//	            switch (x) {
+//	                case 1:
+//	                    System.out.println("SCIENCE, MATHS, PHYSICS");
+//	                    break;
+//	                case 2:
+//	                    switch (stream) {
+//	                        case 'A':
+//	                            System.out.println("Welcome");
+//	                            break;
+//	                        case 'C':
+//	                            System.out.println("TechnoStudy");
+//	                            break;
+//	                        case 'B':
+//	                            System.out.println("Have a nice day");
+//	                            break;
+//	                    }
+//	                    break;
+//	                case 3:
+//	                    switch (stream) {
+//	                        case 'C':
+//	                            System.out.println("Welcome");
+//	                            break;
+//	                        case 'D':
+//	                            System.out.println("In");
+//	                            break;
+//	                        case 'E':
+//	                            System.out.println("GFG");
+//	                            break;
+//	                    }
+//	                    break;
+//	            }
+//	        }
+//	    }
+////	    Options:
+	////1. Compile time error
+	////2. GFG
+	////3. No Output
+	////4. TechnoStudy
+		
+
+//		Answer:4
+
+	
