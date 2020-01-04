b8ed41ba95d76a2476a6bf4c600862c9246741db
yvzydn <yavuzaydinus@gmail.com>
2020-01-03 21:33:51 -0500

    Day13 Optional Hw added

--- TechnoHomework/src/com/selenium/SeleniumHW2.java
+++ TechnoHomework/src/com/selenium/SeleniumHW2.java
@@ -10,44 +10,45 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumHW2 {
 
 	public static void main(String[] args) {
+		
 		System.setProperty("webdriver.chrome.driver", "/Users/yavuzaydin/Documents/Libraries/drivers/chromedriver");
 
 		WebDriver driver = new ChromeDriver();
 
 		driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+		
 		driver.findElement(By.className("dropdown-toggle")).click();
-	    driver.findElement(By.linkText("Checkbox Demo")).click();
+		
+	  //  driver.findElement(By.linkText("Checkbox Demo")).click();
 	    
-	    List<WebElement> option = driver.findElements( By.className( "cb1-element" ) );
-	    
-	    for(int i=0;i<option.size();i++) {
-	    	
-         option.get( i ).click();
-              	    
-	}	    	
-	    
-	   String text=driver.findElement(By.id("check1")).getAttribute("value");
-	     
-	   
-	   if(text.equals("Uncheck All")) {
-		   System.out.println("Pass");
-	   }else {
-		   System.out.println("Fail");
-	   }
-	   
-	    for(int i=0;i<option.size();i++) {
-	    	
-	         option.get( i ).click();
-	         
-	       text=driver.findElement(By.id("check1")).getAttribute("value");
-	       
-	       if(text.equals("Check All")) {
-			   System.out.println("Pass");
-		   }else {
-			   System.out.println("Fail");
-		   }
-	              	    
-		}
-	   
+//	    List<WebElement> option = driver.findElements( By.className( "cb1-element" ) );
+//	    
+//	    for(int i=0;i<option.size();i++) {
+//	    	
+//         option.get( i ).click();
+//              	    
+//	}	    	    
+//	   String text=driver.findElement(By.id("check1")).getAttribute("value");
+//	     	   
+//	   if(text.equals("Uncheck All")) {
+//		   System.out.println("Pass");
+//	   }else {
+//		   System.out.println("Fail");
+//	   }
+//	   
+//	    for(int i=0;i<option.size();i++) {
+//	    	
+//	         option.get( i ).click();
+//	         
+//	       text=driver.findElement(By.id("check1")).getAttribute("value");
+//	       
+//	       if(text.equals("Check All")) {
+//			   System.out.println("Pass");
+//		   }else {
+//			   System.out.println("Fail");
+//		   }
+//	              	    
+//		}
+//	   
 	}
 }
